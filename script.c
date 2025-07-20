038f7e9fce6159b84c0110f71a8468002b1c3e56
Fabrício Fraga Rubim <fabriciofraga200618@gmail.com>
2025-07-19 22:42:47 -0300

    Add files via upload

---
+++ script.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    int n = 14; 
+
+    if (n < 10)
+    {
+        printf("numero pequeno\n");
+    }
+    else if(n <= 10)
+    {
+        printf("Numero mediano\n");
+    }
+
+    else if (n <= 15)
+    {
+        printf("Numero um pouco grande\n");
+    }
+
+    else
+    {
+        printf("Numero grande\n");
+    }
+}
